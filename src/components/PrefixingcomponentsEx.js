b262d73dc94ee96f774be8e797249cd123be70fd
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-12-29 14:29:42 +0200

    Project started still on layout stack

---
+++ src/components/PrefixingcomponentsEx.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ThemeProvider from 'react-bootstrap/ThemeProvider';
+import Button from 'react-bootstrap/Button';
+
+function PrefixingcomponentsEx() {
+    return (
+        <>
+          {/* Hint: inspect the markup to see how the classes differ */}
+          <ThemeProvider prefixes={{ btn: 'my-btn' }}>
+            <Button variant="primary">My Button</Button>
+          </ThemeProvider>{' '}
+          <Button bsPrefix="super-btn" variant="primary">
+            Super button
+          </Button>
+        </>
+      );
+}
+
+export default PrefixingcomponentsEx
